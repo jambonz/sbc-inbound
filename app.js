2af3d13e10c14c6592661ff3e53bd7426f8b1b5a
Dave Horton <daveh@beachdognet.com>
2019-12-19 09:39:32 -0500

    logging

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ if (config.has('drachtio.host')) {
   });
 }
 else {
-  logger.info(`listening for drachtio server traffic on ${config.get('drachtio')}`);
+  logger.info(`listening for drachtio server traffic on ${JSON.stringify(config.get('drachtio'))}`);
   srf.listen(config.get('drachtio'));
 }
 if (process.env.NODE_ENV === 'test') {
