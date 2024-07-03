f17ae9389bb5c4c050d3acdea65cfc98a785dcb4
Dave Horton <daveh@beachdognet.com>
2024-07-03 10:14:40 -0600

    fix for k8s

--- app.js
+++ app.js
@@ -137,6 +137,7 @@ const {
   challengeDeviceCalls
 } = require('./lib/middleware')(srf, logger);
 const CallSession = require('./lib/call-session');
+const { parse } = require('path');
 
 if (process.env.DRACHTIO_HOST && !process.env.K8S) {
   const cidrs = process.env.JAMBONES_NETWORK_CIDR
@@ -167,7 +168,6 @@ if (process.env.DRACHTIO_HOST && !process.env.K8S) {
         }
       }
     }
-    srf.locals.sbcPublicIpAddress = {};
     for (const hp of hostports) {
       const arr = /^(.*)\/(.*):(\d+)$/.exec(hp);
       if (arr && 'udp' === arr[1] && !matcher.contains(arr[2])) {
@@ -183,34 +183,8 @@ if (process.env.DRACHTIO_HOST && !process.env.K8S) {
         srf.locals.removeFromRedis = () => removeFromSet(setName, hostport);
         srf.locals.addToRedis();
       }
-      if (arr) {
-        const ipv4 = arr[2];
-        const port = arr[3];
-        switch (arr[1]) {
-          case 'udp':
-            srf.locals.sbcPublicIpAddress = {
-              ...srf.locals.sbcPublicIpAddress,
-              udp: `${ipv4}:${port}`
-            };
-            break;
-          case 'tls':
-            srf.locals.sbcPublicIpAddress = {
-              ...srf.locals.sbcPublicIpAddress,
-              tls: `${ipv4}:${port}`
-            };
-            break;
-          case 'wss':
-            srf.locals.sbcPublicIpAddress = {
-              ...srf.locals.sbcPublicIpAddress,
-              wss: `${ipv4}:${port}`
-            };
-            break;
-        }
-      }
-      if (!srf.locals.sbcPublicIpAddress.tls) {
-        srf.locals.sbcPublicIpAddress.tls = `${srf.locals.sipAddress}:5061`;
-      }
     }
+    srf.locals.sbcPublicIpAddress = parseHostPorts(hostports);
   });
 }
 else {
@@ -234,6 +208,7 @@ else {
         }
       }
     }
+    srf.locals.sbcPublicIpAddress = parseHostPorts(hp.split(','));
   });
 }
 if (process.env.NODE_ENV === 'test') {
@@ -389,5 +364,41 @@ function handle(removeFromSet, setName, signal) {
     }
   }
 }
+
+const parseHostPorts = (hostports) => {
+  let obj = {}; 
+  for (const hp of hostports) {
+    const arr = /^(.*)\/(.*):(\d+)$/.exec(hp);
+    if (arr) {
+      const ipv4 = arr[2];
+      const port = arr[3];
+      switch (arr[1]) {
+        case 'udp':
+          obj = {
+            ...obj,
+            udp: `${ipv4}:${port}`
+          };
+          break;
+        case 'tls':
+          obj = {
+            ...obj,
+            tls: `${ipv4}:${port}`
+          };
+          break;
+        case 'wss':
+          obj = {
+            ...obj,
+            wss: `${ipv4}:${port}`
+          };
+          break;
+      }
+    }
+    if (!obj.tls) {
+      obj.tls = `${srf.locals.sipAddress}:5061`;
+    }
+    logger.info({obj}, 'sip endpoints');
+    return obj;
+  }
+};
 
 module.exports = {srf, logger};
