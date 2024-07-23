6ef86f18f8cc2d77b6a42a14f02fea29ae28a21d
Markus Frindt <m.frindt@cognigy.com>
2024-07-23 14:31:57 +0200

    add test

--- test/utils.js
+++ test/utils.js
@@ -1,5 +1,6 @@
 const test = require('tape');
 const { parseHostPorts } = require('../lib/utils');
+const { parseUri } = require('drachtio-srf');
 
 process.on('unhandledRejection', (reason, p) => {
   console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
@@ -34,6 +35,46 @@ test('utils tests - parseHostPorts', async (t) => {
     obj = parseHostPorts(logger, hostportsNoTls.split(','), srf);
 
     t.ok(obj.tls === '127.0.0.1:5061', 'sip endpoint tls');
+
+    t.end();
+  } catch (err) {
+    console.log(`error received: ${err}`);
+    t.error(err);
+  }
+});
+
+
+test('utils tests - parse URI user', async (t) => {
+  try {
+    const req = {
+      "referTo": { "uri": "sip:@202660.tenios.com" },
+      getParsedHeader: () => ({ "uri": "sip:@202660.tenios.com" })
+    };
+    // "refer-to":"<sip:+49221578952870@202660.tenios.com>"
+    // <sip:@202660.tenios.com>
+
+    const referTo = req.getParsedHeader('Refer-To');
+    const uri = parseUri(referTo.uri);
+    // uri.user does not exist
+    const arr = /context-(.*)/.exec(uri.user);
+
+    const expected = {
+      family: "ipv4",
+      scheme: "sip",
+      user: "",
+      password: undefined,
+      host: "202660.tenios.com",
+      port: NaN,
+      params: {},
+      headers: {},
+    }
+
+    t.ok(uri.family === expected.family, 'sip endpoint tls');
+    t.ok(uri.scheme === expected.scheme, 'sip endpoint wss');
+    t.ok(uri.password === expected.password, 'sip endpoint tcp');
+    t.ok(uri.host === expected.host, 'sip endpoint udp');    
+    t.ok(typeof uri.params === 'object', 'sip endpoint udp');
+    t.ok(typeof uri.headers === 'object', 'sip endpoint udp');
 
     t.end();
   } catch (err) {
