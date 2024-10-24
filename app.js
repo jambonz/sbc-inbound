dceb191c14df4f6c713dfc816e199efbc4d2d1ca
mfrindt <m.frindt@cognigy.com>
2024-10-24 09:35:40 +0200

    fix 2 low vulnerabilities, centralize env vars validation

--- app.js
+++ app.js
@@ -1,18 +1,4 @@
-const assert = require('assert');
-assert.ok(process.env.JAMBONES_MYSQL_HOST &&
-  process.env.JAMBONES_MYSQL_USER &&
-  process.env.JAMBONES_MYSQL_PASSWORD &&
-  process.env.JAMBONES_MYSQL_DATABASE, 'missing JAMBONES_MYSQL_XXX env vars');
-if (process.env.JAMBONES_REDIS_SENTINELS) {
-  assert.ok(process.env.JAMBONES_REDIS_SENTINEL_MASTER_NAME,
-    'missing JAMBONES_REDIS_SENTINEL_MASTER_NAME env var, JAMBONES_REDIS_SENTINEL_PASSWORD env var is optional');
-} else {
-  assert.ok(process.env.JAMBONES_REDIS_HOST, 'missing JAMBONES_REDIS_HOST env var');
-}
-assert.ok(process.env.DRACHTIO_PORT || process.env.DRACHTIO_HOST, 'missing DRACHTIO_PORT env vars');
-assert.ok(process.env.DRACHTIO_SECRET, 'missing DRACHTIO_SECRET env var');
-assert.ok(process.env.JAMBONES_TIME_SERIES_HOST, 'missing JAMBONES_TIME_SERIES_HOST env var');
-assert.ok(process.env.JAMBONES_NETWORK_CIDR || process.env.K8S, 'missing JAMBONES_NETWORK_CIDR env var');
+require('./lib/config').validateEnvVars();
 
 const Srf = require('drachtio-srf');
 const srf = new Srf('sbc-inbound');
